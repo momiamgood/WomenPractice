eaf8f8500ad86ba8d083d4c1c3aaa01b509cf7e7
safo4eg <stas.rakov111@mail.ru>
2024-01-07 12:28:21 +0300

    add data

--- Bot_V2.php
+++ Bot_V2.php
@@ -27,13 +27,13 @@ class Bot_V2
 
         // токен телеграм бота
         define('TELEGRAM_TOKEN', $configs['system']['telegram_token']);
-        define("ADMIN_CHAT_ID", -1002120324080);
+        define("ADMIN_CHAT_ID", -1001706592010);
 
         # Подключение к бд
         $mysql_ip = "localhost";
-        $mysql_dbname = "satory";
-        $mysql_dbuser = "root";
-        $mysql_password = "";
+        $mysql_dbname = "aktiviev_katyasa";
+        $mysql_dbuser = "aktiviev_katyasa";
+        $mysql_password = "qWbniV&4";
 
         date_default_timezone_set($configs['system']['timezone']);
 
